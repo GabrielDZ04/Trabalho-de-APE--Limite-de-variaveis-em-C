fa5602818ce1977478ed8b5176f7b00c9e1a8a80
GabrielDZ04 <gabrielppaiva2001@gmail.com>
2024-09-24 12:03:59 -0300

    Primeiros códigos adicionados

---
+++ decoGABRIEL.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h> 
+#include <math.h>
+
+int main() {
+
+        /*for (j = 0; j <=length; j+=2)
+        {
+            dupla[0] = hexadecimal[j];
+            dupla[1] = hexadecimal[j+1];
+            decimal = strtol(dupla, NULL, 16);
+
+            if (func_val(j, b) != 0){
+                printf("%c", decimal);
+            }
+
+            if(decimal == 0){
+                break;
+            }            
+        }
+        
+        printf("\n \n");
+    }*/
+
+    return 0; 
+}
